61d16c6d5d6750c2c5e7297635bdd671a26ada1e
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-02 23:30:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+def nyc_pigeon_organizer(data)
+  reorganized_data = Hash.new 0 
+    names_array = []
+    data.map {|key, value| 
+      value.map {|key2, value2| 
+          names_array << value2 
+      }
+    }
+    names_array = names_array.flatten.uniq 
+
+    names_array.map {|name| reorganized_data[name] = {}}
+    attribute_array = []
+    data.map {|key, value|
+      attribute_array << key 
+    }
+    attribute_hash = attribute_array.to_h
+    p attribute_hash
+  reorganized_data.map {|key, value|
+   
+      
+    }
+  
+  
+    #reorganized_data.map {|key, value| 
+      #value.merge!()
+    #}
+  return reorganized_data
+end
 end
