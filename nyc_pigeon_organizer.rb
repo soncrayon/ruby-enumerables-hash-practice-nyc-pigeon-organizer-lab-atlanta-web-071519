363d134fe25733803f55829a9ad7888a8c622432
Curtis Arnold <arnold.curtis@gmail.com>
2019-07-03 00:08:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  reorg_list = {}
+  data.map { |trait, attributes|
+    attributes.map { |details, names|
+      names.map { |name|
+        reorg_list[name] ||= {}
+        reorg_list[name][trait] ||= []
+        reorg_list[name][trait] << details.to_s
+      }
+    }
+  }
+  return reorg_list
 end
